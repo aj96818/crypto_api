4b1244685f51c95e5d3598471daab4a14f4c5675
aj96818 <aj96818@gmail.com>
2021-03-26 14:28:54 -0400

    overwritten original version using _test.py version

--- coin_basic_api.py
+++ coin_basic_api.py
@@ -1,29 +1,19 @@
-  
- #This example uses Python 2.7 and the python-request library.
-
-
-'''
-Steps for execution:
-1) cd to Documents/Environments
-2) activate crypto_api_env python virtual environment in shell:
-	source crypto_api_env/bin/activate
-3) cd into crypto_api_env/crypto_api to move to git repo.
-
-'''
-
-
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
+import pandas as pd
+
 
 api_key = '231f04b7-44ce-4dcd-8dfd-0f0e0e1fbda4'
 
+
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 parameters = {
-  'start':'1',
-  'limit':'5000',
-  'convert':'USD'
+  'start':'1'
+  , 'limit':'5000'
+  , 'convert':'USD'
 }
+
 headers = {
   'Accepts': 'application/json',
   'X-CMC_PRO_API_KEY': api_key,
@@ -35,7 +25,19 @@ session.headers.update(headers)
 try:
   response = session.get(url, params=parameters)
   data = json.loads(response.text)
-  print(data)
+  df = pd.DataFrame.from_dict(data, orient = 'index')
+  print(df)
 except (ConnectionError, Timeout, TooManyRedirects) as e:
-  print(e)
-  
+  print('except error: check code')
+
+
+		# x = json.dumps(response_json)
+		# d = json.loads(x)
+		# e = d['quarterlyEarnings'][:10]
+		
+		# for dic in e:
+		# 	df = pd.DataFrame.from_dict(dic, orient = 'index')
+		# 	df = df.transpose()
+		# 	df['symbol'] = ticker
+		# 	eps_list.append(df)
+		# 	time.sleep(2)
