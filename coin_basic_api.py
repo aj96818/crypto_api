3f1c9f32d12ceb031b008512bf0591f1678a7b33
aj96818 <aj96818@gmail.com>
2021-03-27 15:04:32 -0400

    Seems to be working fine on Windows Server.  Scheduled to run on Windows Server via Task Scheduler on 3/27/21.

--- coin_basic_api.py
+++ coin_basic_api.py
@@ -31,22 +31,18 @@ try:
 
   df_short = df[['symbol', 'slug', 'date_added', 'last_updated', 'quote.USD.price', 'quote.USD.volume_24h', 'quote.USD.market_cap', 'quote.USD.percent_change_24h', 'quote.USD.percent_change_7d', 'quote.USD.percent_change_30d', 'quote.USD.percent_change_60d', 'quote.USD.percent_change_90d']]
   
-  df_short.to_csv(r'coinmarketcap_api.csv')
+  crypto_tickers = ['CVC', 'LSK', 'OMG', 'BTC', 'ETH', 'NEO', 'ADA', 'OCEAN', 'DOT', 'TRAC', 'MRPH', 'BAL', 'ZRX'
+                    'COMP', 'MKR', 'SNX', 'BNB', 'LINK', 'UNI', 'XMR', 'ALGO', 'UMA', 'REN', 'BAT', 'ONT'
+                    , 'KNC', 'UBT', 'OMI', 'EWT', 'SOL', 'WPR', 'XLM', 'ATOM', 'XRP', 'EOS', 'MANA']
 
-
-
-
-  
-
-
-
-
+  #df_short.crypto_tickers.isin(crypto_tickers)
+  df_out = df_short[df_short['symbol'].isin(crypto_tickers)]
+#  df_out.to_csv(r'coinmarketcap_api_v2.csv')
+  win_path = r'C:\\Users\\aljackson\\Documents\\Environments\\crypto_api\\CoinmarketcapAPI_Crypto_Data.csv'
+  df_out.to_csv(win_path, mode = 'a', index = False, header = False)
 
   # for x in data:
   # 	crypto_list.append(x['symbol'], x['slug'], x['date_added'], x['last_updated'], x['quote']['USD']['price'])
- 
-
-
 #  	print(x['symbol'], x['slug'], x['date_added'], x['last_updated'], x['quote']['USD']['price'])
 
 except (ConnectionError, Timeout, TooManyRedirects) as e:
